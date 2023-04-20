9cd3bb62c574a75eb300000c3d342fd6a15470a9
Pinghie <fabio.melasi@gmail.com>
2023-04-20 16:18:07 +0200

    2 files modified

--- src/app/ricerca/gestione/gestione.component.ts
+++ src/app/ricerca/gestione/gestione.component.ts
@@ -15,20 +15,33 @@ export class GestioneComponent implements OnInit {
     private app: AppComponent
   ) {}
 
-  risultato = this.ricercaComponent.listaRisultante[0];
+  risultato: object = this.ricercaComponent.listaRisultante[0];
+  nuovoArchivio: Array<Object>;
 
   rimozione() {
-    let nuovoArchivio = this.ricercaComponent.archivio.filter(
+    this.rimozioneOggetto();
+    this.aggiornaArchivio();
+  }
+
+  restituzione() {
+    this.rimozioneOggetto();
+    this.risultato['prestato'] = '';
+    console.log(this.risultato);
+  }
+
+  private rimozioneOggetto() {
+    this.nuovoArchivio = this.ricercaComponent.archivio.filter(
       (value) => value['posizione'] != this.risultato['posizione']
     );
-    this.interazione.setData(JSON.stringify(nuovoArchivio)).subscribe({
+  }
+
+  private aggiornaArchivio() {
+    this.interazione.setData(JSON.stringify(this.nuovoArchivio)).subscribe({
       next: () => (this.app.vista = 'home'),
       error: (err) =>
         console.log('Observer got an error: ' + JSON.stringify(err)),
     });
   }
 
-  ngOnInit() {
-    console.log(this.ricercaComponent.archivio);
-  }
+  ngOnInit() {}
 }
