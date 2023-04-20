38d694197631d3a8bd08bbd3ecb6baa23c63fd64
Pinghie <fabio.melasi@gmail.com>
2023-04-20 16:00:16 +0200

    1 file modified

--- src/app/ricerca/gestione/gestione.component.ts
+++ src/app/ricerca/gestione/gestione.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RicercaComponent } from '../ricerca.component';
+import { servizioDatabase } from '../../app.service';
 
 @Component({
   selector: 'app-gestione',
@@ -7,18 +8,22 @@ import { RicercaComponent } from '../ricerca.component';
   styleUrls: ['./gestione.component.css'],
 })
 export class GestioneComponent implements OnInit {
-  constructor(private ricercaComponent: RicercaComponent) {}
+  constructor(
+    private ricercaComponent: RicercaComponent,
+    private interazione: servizioDatabase
+  ) {}
 
   risultato = this.ricercaComponent.listaRisultante[0];
 
   rimozione() {
-    console.log('AAA');
-    this.risultato['posizione'];
-    console.log(this.ricercaComponent.archivio);
     let nuovoArchivio = this.ricercaComponent.archivio.filter(
       (value) => value['posizione'] != this.risultato['posizione']
     );
-    console.log(nuovoArchivio);
+    this.interazione.setData(JSON.stringify(nuovoArchivio)).subscribe({
+      next: () => console.log('andata'),
+      error: (err) =>
+        console.log('Observer got an error: ' + JSON.stringify(err)),
+    });
   }
 
   ngOnInit() {
