d7cb74b83c80a8c3fa595b6bfa3ab3e954494819
Pinghie <fabio.melasi@gmail.com>
2023-04-20 10:14:41 +0200

    1 file modified

--- src/app/inserimento/inserimento.component.ts
+++ src/app/inserimento/inserimento.component.ts
@@ -10,7 +10,7 @@ export class InserimentoComponent implements OnInit {
   constructor(private interazione: servizioDatabase) {}
 
   inserimento(autore: string, titolo: string, posizione: string) {
-    console.log(autore, titolo, posizione);
+    //console.log(autore, titolo, posizione);
     let aggiunta =
       ',{"autore":"' +
       autore +
@@ -25,11 +25,13 @@ export class InserimentoComponent implements OnInit {
       next: (x: any) => {
         x = x.substring(0, x.length - 1) + aggiunta;
         console.log(x);
-        this.interazione.setData(x).subscribe({
+        console.log(JSON.parse(x));
+        console.log(JSON.stringify(JSON.parse(x)));
+        /*this.interazione.setData(x).subscribe({
           next: (y: any) => console.log('andata'),
           error: (err) =>
             console.log('1Observer got an error: ' + JSON.stringify(err)),
-        });
+        });*/
       },
       error: (err) =>
         console.error('2Observer got an error: ' + JSON.stringify(err)),
