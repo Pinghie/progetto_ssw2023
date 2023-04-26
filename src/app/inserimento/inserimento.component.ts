7069f53595ef6c222f119ec0535d3bc7dba31957
Pinghie <fabio.melasi@gmail.com>
2023-04-26 10:41:28 +0200

    1 file modified

--- src/app/inserimento/inserimento.component.ts
+++ src/app/inserimento/inserimento.component.ts
@@ -16,29 +16,33 @@ export class InserimentoComponent {
   errore: string = '';
 
   inserimento(autore: string, titolo: string, posizione: string) {
-    let archivio: Array<Libro>;
-    const libro = new Libro(autore, titolo, posizione);
+    if (autore !== '' && titolo !== '' && posizione !== '') {
+      let archivio: Array<Libro>;
+      const libro = new Libro(autore, titolo, posizione);
 
-    this.interazione.getData().subscribe({
-      next: (stringaArchivio: string) => {
-        archivio = JSON.parse(stringaArchivio);
-        if (archivio.every((value) => value['posizione'] !== libro.posizione)) {
-          archivio.push(libro);
-          this.interazione.setData(JSON.stringify(archivio)).subscribe({
-            next: () => (this.app.vista = 'home'),
-            error: (err) =>
-              console.error(
-                "Errore nell'Observer SET in inserimento.component: " +
-                  JSON.stringify(err)
-              ),
-          });
-        } else this.errore = 'Esiste già un libro in questa posizione';
-      },
-      error: (err) =>
-        console.error(
-          "Errore nell'Observer GET in inserimento.component: " +
-            JSON.stringify(err)
-        ),
-    });
+      this.interazione.getData().subscribe({
+        next: (stringaArchivio: string) => {
+          archivio = JSON.parse(stringaArchivio);
+          if (
+            archivio.every((value) => value['posizione'] !== libro.posizione)
+          ) {
+            archivio.push(libro);
+            this.interazione.setData(JSON.stringify(archivio)).subscribe({
+              next: () => (this.app.vista = 'home'),
+              error: (err) =>
+                console.error(
+                  "Errore nell'Observer SET in inserimento.component: " +
+                    JSON.stringify(err)
+                ),
+            });
+          } else this.errore = 'Esiste già un libro in questa posizione';
+        },
+        error: (err) =>
+          console.error(
+            "Errore nell'Observer GET in inserimento.component: " +
+              JSON.stringify(err)
+          ),
+      });
+    } else this.errore = 'Tutti i campi devono essere compilati';
   }
 }
